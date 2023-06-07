3faedd6ee5e69dceb9f1aa75a54b6169daeb99c5
Max2288 <kennySRU@ya.ru>
2023-06-07 14:19:05 +0300

    Try again

--- RussianRailways/tickets/signals.py
+++ RussianRailways/tickets/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from . import models
 
-@receiver(post_save, sender=models.HumanTicket)
+@receiver(post_save, sender=models.Ticket)
 def handle_cancelled_ticket(instance, **kwargs):
     if instance.status == 'Cancelled':
         instance.delete()
